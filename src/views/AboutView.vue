033a6206a10f1f7dbdcede3f02254b39ca28be19
khang nguyen <khangnguyena41905@gmail.com>
2023-11-07 16:28:45 +0700

    add bootstrap 5

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
+  <div id="about-view" class="about">
     <h1>This is an about page</h1>
+    <h1 class="text-danger">TEST BOOTSTRAP 5</h1>
+    <div class="">
+      <h1 class="test-color bg_light-purple">TEST GLOBAL Variable</h1>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+#about-view {
+  .test-color {
+    color: $dark-blue;
+  }
+}
+</style>
